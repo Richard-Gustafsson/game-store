7bc6bbd988451d40d7e801875be641759052f18c
Richard Gustafsson <rille117@hotmail.com>
2018-02-22 16:49:59 +0100

    Added bootstrap to the project

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from 'reactstrap';
 
 export default class App extends Component {
   render() {
     return(
       <div>
         <h1>Hello world!</h1>
+        <button>Hej</button>
+        <Button color="danger">Danger!</Button>
       </div>
     );
   }
--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+import 'bootstrap/dist/css/bootstrap.css';
 import App from '../App';
 
 ReactDOM.render(<App />, document.getElementById('app'))
